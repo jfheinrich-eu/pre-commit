2a88333c4c8a5d3fd380f01522642713fdb9c01e
Joerg Heinrich <joerg@jfheinrich.eu>
2025-03-24 06:37:59 +0100

    feat(entrypoint): refactor internal option handling
    Changelog: changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -79,15 +79,15 @@ done
 # shellcheck disable=SC2086
 # shellcheck disable=SC2048
 if [ "$CI" = "true" ]; then
-  [[ -z "$update_hooks" ]] || pre-commit autoupdate --repo https://github.com/pre-commit/pre-commit-hooks $update_hooks
   pre-commit $* $cmd_append
 else
   if [ "$image_help" = "true" ]; then
     usage
   elif [ "$copy_config" = "true" ]; then
     cp /root/.pre-commit-config.yaml /builds/.pre-commit-config-example.yaml && echo "Example config host://.pre-commit-config-example.yaml created."
+  elif [ -n "$update_hooks" ]; then
+    pre-commit autoupdate --config $update_hooks
   else
-    [[ -z "$update_hooks" ]] || pre-commit autoupdate --repo https://github.com/pre-commit/pre-commit-hooks $update_hooks
     cd /builds && git config --global --add safe.directory /builds && pre-commit $* $cmd_append
   fi
 fi
