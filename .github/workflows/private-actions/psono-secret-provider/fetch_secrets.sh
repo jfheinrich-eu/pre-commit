85e87cef2c282f89d6f1576baa0be736b6b21914
Joerg Heinrich <joerg@jfheinrich.eu>
2025-04-08 15:45:04 +0200

    feat(github action): add private action to fetch the secrets from PSONO server
    Changelog: added

---
+++ .github/workflows/private-actions/psono-secret-provider/fetch_secrets.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+$PROG=$(dirname $0)/psonoci
+
+case "$SECRET_TYPE" in
+env)
+    command="env-vars get-or-create"
+    break
+    ;;
+secret)
+    command="secret get"
+    break
+    ;;
+esac
+
+for f in $(echo $SECRET_FIELDS |  tr ',' ' '); do
+    SECRET_VALUE_NAME=$f
+
+    $fetched_secret_name="${SECRET_VALUE_NAME}_fetched"
+
+    ORG_IFS=${IFS}
+    IFS= read -r -d '' "$fetched_secret_name" <<<"$(${PROG} ${command} ${SECRET_ID} {SECRET_VALUE_NAME})"
+    IFS=${ORG_IFS}
+
+    for m in $(echo $MASK_SECRET | tr ',' ' '); do
+        if [ "$m" == "$SECRET_VALUE_NAME" ]; then
+            echo "::add-mask::${fetched_seceret_name}"
+        fi
+    done
+
+    echo "${SECRET_VALUE_NAME}=${fetched_secret_name}" >>"$GITHUB_OUTPUT"
+done
