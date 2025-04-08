c8efb46e8a23e4a03b5bd41e332da97623a48014
Joerg Heinrich <joerg@jfheinrich.eu>
2025-04-08 22:27:17 +0200

    feat(psono github action): setup the arguments for the image

--- .github/workflows/private-actions/psono-secret-provider/entrypoint.sh
+++ .github/workflows/private-actions/psono-secret-provider/entrypoint.sh
@@ -6,6 +6,7 @@ PSONO_CI_API_KEY_ID="$INPUT_CI_API_KEY_ID"
 PSONO_CI_API_SECRET_KEY_HEX="$INPUT_CI_API_SECRET_KEY_HEX"
 PSONO_CI_SERVER_URL="$INPUT_CI_SERVER_URL"
 SECRET_TYPE="$INPUT_SECRET_TYPE"
+SECRET_ID="$INPUT_SECRET_ID"
 SECRET_FIELDS=($INPUT_SECRET_FIELDS)
 MASK_SECRETS=($INPUT_MASK_SECRETS)
 
@@ -19,10 +20,10 @@ MASK_SECRETS=($INPUT_MASK_SECRETS)
 
 case "$SECRET_TYPE" in
 env)
-    command="env-vars get-or-create"
+    command=(env-vars get-or-create)
     ;;
 secret)
-    command="secret get"
+    command=(secret get)
     ;;
 *)
     echo "Unknown SECRET_TYPE: $SECRET_TYPE"
@@ -30,14 +31,14 @@ secret)
     ;;
 esac
 
-for f in "${SECRET_FIELDS[@]}"; do
+for f in ${SECRET_FIELDS[@]}; do
     SECRET_VALUE_NAME=$f
 
     fetched_secret_name="${SECRET_VALUE_NAME}_fetched"
 
-    IFS= read -r -d '' "$fetched_secret_name" <<<"$(${PROG} ${command} ${SECRET_ID} ${SECRET_VALUE_NAME})"
+    IFS= read -r -d '' "$fetched_secret_name" <<<$(${PROG} ${command[0]} ${command[1]} ${SECRET_ID} ${SECRET_VALUE_NAME})
 
-    for m in "${MASK_SECRETS[@]}"; do
+    for m in ${MASK_SECRETS}; do
         if [ "$m" == "$SECRET_VALUE_NAME" ]; then
             echo "::add-mask::${fetched_secret_name}"
         fi
